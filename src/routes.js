4406b1b621ff44767c2471d4796aa768e30214b5
Joao Pedro Hartmann Salomao <jpsalomao@MacBook-Air-de-Joao.local>
2018-11-26 19:32:33 -0200

    Added authentication

--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,23 @@ const upload = require('multer')(multerConfig)
 
 const routes = express.Router()
 
+const authMiddleware = require('./app/middlewares/auth')
+const guestMiddleware = require('./app/middlewares/guest')
 const UserController = require('./app/controllers/UserController')
-routes.get('/signup', UserController.create)
+const SessionController = require('./app/controllers/SessionController')
+
+routes.get('/', guestMiddleware, SessionController.create)
+routes.post('/signin', SessionController.store)
+
+routes.get('/signup', guestMiddleware, UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
+
+routes.use('/app', authMiddleware)
+
+routes.get('/app/logout', SessionController.destroy)
+routes.get('/app/dashboard', (req, res) => {
+  console.log(req.session.user)
+  return res.render('dashboard')
+})
 
 module.exports = routes
