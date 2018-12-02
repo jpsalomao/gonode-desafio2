0e54cef4d5c0ef3099971d71d3b7bf1b22e640af
Joao Pedro Hartmann Salomao <jpsalomao@MacBook-Air-de-Joao.local>
2018-12-01 22:57:16 -0200

    Created new appointment form

--- src/routes.js
+++ src/routes.js
@@ -10,6 +10,7 @@ const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 const DashboardController = require('./app/controllers/DashboardController')
 const FileController = require('./app/controllers/FileController')
+const AppointmentController = require('./app/controllers/AppointmentController')
 
 routes.use((req, res, next) => {
   res.locals.flashSuccess = req.flash('success')
@@ -31,4 +32,5 @@ routes.get('/app/logout', SessionController.destroy)
 
 routes.get('/app/dashboard', DashboardController.index)
 
+routes.get('/app/appointments/new/:provider', AppointmentController.create)
 module.exports = routes
